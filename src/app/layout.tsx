8eda4adfd9d050b8338600f1389d39e35c21c3f1
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-09-30 12:22:17 +0600

    re-init: reinitialize the application setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "GrowSphere",
+  description: "GrowSphere - Gardening made easy",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>{children}</body>
+    </html>
+  );
+}
