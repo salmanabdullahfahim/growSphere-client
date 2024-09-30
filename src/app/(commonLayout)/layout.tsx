ed4a39f7d45cc4a5c0f8d9f94c4458800afcf684
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-09-30 19:07:59 +0600

    UI: added footer

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
 
@@ -6,6 +7,7 @@ const CommonLayout = ({ children }: { children: React.ReactNode }) => {
     <div>
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h2 className="text-3xl text-green-400 text-center">Homepage</h2>
+      <h2 className="text-3xl">Homepage</h2>
     </div>
   );
 };
