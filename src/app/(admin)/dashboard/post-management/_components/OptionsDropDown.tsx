6ca661190de29aaf280986ae5765d735791e7074
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-10-05 15:31:43 +0600

    Complete: Admin Dashboard

--- src/app/(admin)/dashboard/post-management/_components/OptionsDropDown.tsx
+++ src/app/(admin)/dashboard/post-management/_components/OptionsDropDown.tsx
@@ -49,7 +49,7 @@ export function OptionsDropdownMenu({ postId }: { postId: string }) {
             disabled={isDeleting}
           >
             <Trash2 className="mr-2 w-5 h-5" />
-            {isDeleting ? "" : "Delete Post"}
+            {isDeleting ? "Deleting..." : "Delete Post"}
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
         </DropdownMenuGroup>
